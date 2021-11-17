12a60a6e14ab2a071465bcf601fa7e00ad4d7f68
Joann ACHER <jacher@e1r5p13.42.fr>
2021-11-17 16:46:35 +0100

    ML00 ex01

--- ML00/ex01/TinyStatistician.py
+++ ML00/ex01/TinyStatistician.py
@@ -1,49 +1,72 @@
 import numpy as np
+import math
+
+def return_list(x):
+    if not isinstance(x, (list, np.ndarray)):
+        print("MEAN: inputs type error")
+        return None
+    if isinstance(x, np.ndarray):
+        mylist = list(x)
+    else:
+        mylist = x
+    if len(mylist) == 0:
+        return None
+    return mylist
 
 class TinyStatistician(object):
 
     @staticmethod
     def mean(x):
-        if isinstance(x, np.ndarray):
-            mean = 0.0
-            for i in x:
-                if isinstance(i, (float, int)):
-                    mean += i
-                else:
-                    print(i)
-                    print("MEAN - NP: inputs type error")
-                    return None
-            return mean / len(x)     
-        elif isinstance(x, list):
-            mean = 0.0
-            for i in x:
-                if isinstance(i, (float, int)):
-                    mean += i
-                else:
-                    print("MEAN - LIST: inputs type error")
-                    return None
-            return mean / len(x)               
-        else:
+        if not isinstance(x, (list, np.ndarray)):
             print("MEAN: inputs type error")
             return None
+        if isinstance(x, np.ndarray):
+            mynp = x
+        else:
+            mynp = np.array(x)
+        return np.mean(mynp)                   
 
     @staticmethod
     def median(x):
-        pass
-        
+        return TinyStatistician.percentile(x, 50)
+               
     @staticmethod
     def quartile(x):
-        pass
+        return tuple((TinyStatistician.percentile(x, 25), TinyStatistician.percentile(x, 75)))
 
     @staticmethod
-    def percentile(x, p):
-        pass
+    def percentile(x, p): #pas calculer pareil que dans numpy
+        if not isinstance(x, (list, np.ndarray)):
+            print("PERCENILE ", p, " - inputs type error")
+            return None
+        if isinstance(x, list):
+            mylist = x
+        else:
+            mylist = list(x)
+        mylist.sort()
+        index = len(mylist) * (p / 100) - 1
+        iceil = math.ceil(index)    
+        return mylist[iceil]        
     
     @staticmethod
     def var(x):
-        pass
-
+        if not isinstance(x, (list, np.ndarray)):
+            print("VAR: inputs type error")
+            return None
+        if isinstance(x, np.ndarray):
+            mynp = x
+        else:
+            mynp = np.array(x)
+        return np.var(mynp)       
+    
     @staticmethod
     def std(x):
-        pass
+        if not isinstance(x, (list, np.ndarray)):
+            print("STD: inputs type error")
+            return None
+        if isinstance(x, np.ndarray):
+            mynp = x
+        else:
+            mynp = np.array(x)
+        return np.std(mynp)     
 
