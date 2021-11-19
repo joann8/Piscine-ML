00aec997c8872d8ed64280ee2786a3b66fe53147
Joann ACHER <jacher@e1r5p13.42.fr>
2021-11-19 14:17:29 +0100

    ML02 ex02

---
+++ ML02/ex02/loss.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def check_dim_maxtrix(mat):
+    if isinstance(mat, np.ndarray):
+        if np.ndim(mat) == 1:
+            mat = np.reshape(mat, (mat.shape[0], 1))       
+        if mat.shape[0] > 0 and mat.shape[1] > 0:
+            return mat
+    return None
+
+def loss_(y, y_hat):
+    """Computes the mean squared error of two non-empty numpy.array, without any for loop.
+    The two arrays must have the same shapes.
+    Args:
+        y: has to be an numpy.array, a vector.
+        y_hat: has to be an numpy.array, a vector.
+    Return:
+        The mean squared error of the two vectors as a float.
+        None if y or y_hat are empty numpy.array.
+        None if y and y_hat does not share the same shapes.
+        None if y or y_hat is not of expected type.
+    Raises:
+        This function should not raise any Exception.
+    """
+    y = check_dim_maxtrix(y)
+    y_hat= check_dim_maxtrix (y_hat)
+    if y is not None and y_hat is not None and y.shape == y_hat.shape:
+        tmp = ((y_hat - y) **2) * (1 / (2 * y.shape[0]))
+        return np.sum(tmp)
+    return None
---
+++ ML02/ex02/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+from loss import loss_
+
+X = np.array([0, 15, -9, 7, 12, 3, -21])
+Y = np.array([2, 14, -13, 5, 12, 4, -19])
+
+print(loss_(X, Y))
+print(loss_(X, X))
